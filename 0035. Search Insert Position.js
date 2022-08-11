5a23ec421a80ebde55d586a27ff3457cf2571bf1
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-08-11 22:09:09 +0530

    0035. Search Insert Position.js

---
+++ 0035. Search Insert Position.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  var len = nums.length;
+  var left = 0;
+  var right = len - 1;
+  var mid = 0;
+
+  if (!len) return 0;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return nums[mid] > target ? mid : mid + 1;
+};
